d5c57d9259fbde70fdecc7beafbfd3f8fc53d1a9
shimohi <saitouyoshikazu@saitouyoshikazunoiMac.local>
2017-06-03 09:53:30 +0900

    Update api

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,15 @@ import {Updater} from "./Updator";
 import {history} from "@s2study/draw-api";
 import DrawHistory = history.DrawHistory;
 
-export function createInstance(
-	history: DrawHistory,
-	editorLayerId?: string
-): Updater {
-	return new Updater(history, editorLayerId);
+export class UpdaterFactory {
+	static createInstance(
+		history: DrawHistory,
+		editorLayerId?: string
+	): Updater {
+		return new Updater(history, editorLayerId);
+	}
 }
-export default createInstance;
+export default UpdaterFactory;
 
 
 /**
